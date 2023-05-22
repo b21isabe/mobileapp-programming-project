0367b49b97a9299b6cdd68e86d17a64e2e9d5558
Isak Bergström <b21isabe@student.his.se>
2023-05-22 02:42:38 +0200

    Skapade en planets arraylist och lade till recyclerview/adapter medlemsvariabler

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -4,8 +4,15 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Planet> planets = new ArrayList<>();
+    RecyclerView view;
+    RecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
