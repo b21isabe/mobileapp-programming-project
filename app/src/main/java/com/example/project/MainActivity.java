463ff6c5a1005b9687539d95c0ba1e1ad3001b45
Isak Bergström <b21isabe@student.his.se>
2023-05-23 02:59:24 +0200

    länkade knappen på startsidan till aboutsidan

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,6 +36,15 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
 
         view = findViewById(R.id.planetRecycler_view);
         new JsonTask(this).execute(JSON_URL);
+
+        Button aboutButton = findViewById(R.id.aboutbutton);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
